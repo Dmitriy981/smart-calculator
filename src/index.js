ba7943f8c2bdbdfbf9d7c8a563c90debc3515b17
Dmitry <981.m.dmitriy@gmail.com>
2018-03-08 02:24:00 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.values = [initialValue];
+        return this;
+    }
 
-  add(number) {
-    // your implementation
-  }
+    add(number) {
+        this.values.push('+', number);
+        return this;
+    }
   
-  subtract(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.values.push('-', number);
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.values.push('*', number);
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    devide(number) {
+        this.values.push('/', number);
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    pow(number) {
+        if(number == 1) {
+            this.values.push("*", 1);
+        } else {
+            this.values.push(Math.pow(this.values.pop(), number));
+        }
+        
+        return this;
+    }
+    
+    valueOf() {
+        return eval(this.values.join(''));
+    }
 }
 
 module.exports = SmartCalculator;
